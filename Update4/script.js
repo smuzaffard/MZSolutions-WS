7dca183f174a34104cab6c521c1a638858e40545
Syed Muzaffar <syed.muzaffar@yahoo.com>
2026-01-05 10:26:48 +0400

    Add files via upload
    Update4folder512026

---
+++ Update4/script.js
@@ -0,0 +1,9 @@
+// Smooth scroll when clicking menu buttons
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        document.querySelector(link.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
